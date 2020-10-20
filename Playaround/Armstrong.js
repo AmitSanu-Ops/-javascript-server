efbca8ae48b88132ed875b87fe5eabff67b8f225
AmitSanu-Ops <72742883+AmitSanu-Ops@users.noreply.github.com>
2020-10-20 14:28:49 +0530

    docs: modified playaround

--- Playaround/Armstrong.js
+++ Playaround/Armstrong.js
@@ -1,9 +1,16 @@
+
+function isArmstrongNumber(n) {
+
+
 let sum = 0;
-let n=121;
+//let n=1634;
 let temp = n;
+let d = n.toString().length; 
+//console.log(d);
 while (temp > 0) {
+    
     let r = temp % 10;
-    sum += r * r * r;
+    sum += Math.pow(r,d);
     temp = parseInt(temp / 10);
 }
 if (sum == n) {
@@ -12,3 +19,8 @@ if (sum == n) {
 else {
     console.log(`${n} is not an Armstrong number.`);
 }
+}
+isArmstrongNumber(1634)
+isArmstrongNumber(153)
+isArmstrongNumber(92727)
+isArmstrongNumber(121)
--- Playaround/factorial.js
+++ Playaround/factorial.js
@@ -1,8 +1,12 @@
+let n;
 function factorial(n)
  {
-    if((n == 0) || (n == 1))
+    if (n < 0) 
+    console.log('Error! Factorial for negative number does not exist.');
+    else if((n == 0) || (n == 1))
       return 1;
     else
       return(n*factorial(n - 1));
   }
-console.log(factorial(12))
+console.log(factorial(-1))
+
