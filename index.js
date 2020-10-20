252b964062d9dfbb596b425c8e432dbebc48c96f
AmitSanu-Ops <72742883+AmitSanu-Ops@users.noreply.github.com>
2020-10-20 14:54:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
+// let is block-scoped and variables declared 
+//within a block of code cannot be accessed outside of it.
+
+//let var
+function begin () {
+    for (var i=0; i<5; i++) {
+     console.log(i) 
+    }
+   // i is accessible outside the for loop
+    console.log.begin(i)
+   }
+
+
+// var
+function begin () {
+    for (var j=0; j<5; j++) {
+     console.log(j) 
+    }
+   // i is accessible outside the for loop
+    console.log(j)
+   }
+
+const user = {"name": "Amit", "age": 20}
+//Can manipulate object properties
+//user.name = "Sanu";
+
+//Cannot re-assign the entire object
+//user = {"name": "Sanu", "age": "28"}
+console.log(user)
+
+//index
 let a = ['first', 'second', 'third', 'fourth'];
 let position = a.indexOf('second');
 console.log("a:", a);
