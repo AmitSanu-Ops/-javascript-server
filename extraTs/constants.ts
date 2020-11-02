fb0dcfeb230dd1464dc67244b90a950259c38faa
AmitSanu-Ops <72742883+AmitSanu-Ops@users.noreply.github.com>
2020-11-02 19:32:33 +0530

    RConflict new

--- extraTs/constants.ts
+++ extraTs/constants.ts
@@ -14,12 +14,7 @@ import { Ipermissions, Iusers} from "./interfaces"
     }
 };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-import { Ipermissions, Iusers} from "./interfaces"
-=======
 
->>>>>>> 685ebccbf113f2d66eeb09b33c1dfbf0ed589912
 
 // let users:Iusers = [
 //   {traineeEmail: 'TRAINER@successive.tech',reviewerEmail: 'reviewer1@successive.tech'
@@ -29,8 +24,6 @@ import { Ipermissions, Iusers} from "./interfaces"
 // {traineeEmail: 'trainee1@successive.tech',reviewerEmail: 'reviewer1@successive.TECH'
 // }
 // ];
-<<<<<<< HEAD
-=======
 export const users = [
   {traineeEmail: 'TRAINER@successive.tech',reviewerEmail: 'reviewer1@successive.tech'
 },
@@ -39,7 +32,4 @@ export const users = [
 {traineeEmail: 'trainee1@successive.tech',reviewerEmail: 'reviewer1@successive.TECH'
 }
 ];
- 
->>>>>>> acde521381eb812244e8bbad2371970cd9ddef09
-=======
->>>>>>> 685ebccbf113f2d66eeb09b33c1dfbf0ed589912
+
